51a717127e6b7686046a3d93007e06c04e34ddda
Md Altaf Hoshain <altaf@squaregroup.com>
2024-05-22 12:39:46 +0600

    Initial Commit

---
+++ EDCR Supervisor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EDCR Supervisor
+//
+//  Created by Md Altaf Hoshain Firoj on 22/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
