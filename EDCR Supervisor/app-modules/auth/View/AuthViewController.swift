cfc645531ac47afd7c7774b111a2d823c3c5bd59
Md Altaf Hoshain <altaf@squaregroup.com>
2024-05-26 17:15:16 +0600

    Login UI design is updated

--- EDCR Supervisor/app-modules/auth/View/AuthViewController.swift
+++ EDCR Supervisor/app-modules/auth/View/AuthViewController.swift
@@ -12,15 +12,40 @@ class AuthViewController: UIViewController {
     var presenter: ViewToPresenterAuthModuleProtocol?
     
     
+    @IBOutlet weak var headerLabel: UILabel!
     
+    @IBOutlet weak var inputUserID: UITextField!
+    
+    @IBOutlet weak var inputPassword: UITextField!
+    
+    @IBOutlet weak var versionLabel: UILabel!
     
     @IBAction func login(_ sender: UIButton) {
-        presenter?.didPerformedLoginOperation(userId: "RM1", userPassword: "RM1")
+        
+        let userId = inputUserID.text
+        let userPassword = inputPassword.text
+        
+        guard let userId = userId, !userId.isEmpty, let userPassword = userPassword, !userPassword.isEmpty else {
+            showAlert(message: "Please enter both UserID and Password.")
+            return
+        }
+        
+        print("UserID:\(userId), userPassword:\(userPassword)")
+    
+        presenter?.didPerformedLoginOperation(userId: userId, userPassword: userPassword)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
+    }
+    
+    func showAlert(message: String) {
+            let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
     }
 
 }
